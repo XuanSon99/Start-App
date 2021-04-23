1251eb845713ea0957a1a9843a10abf1ccce32f6
XuanSon99 <xuansonn12499@gmail.com>
2021-04-23 12:14:09 +0700

    change

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain, session } = require('electron')
 const path = require('path')
+const FB = require('fb')
 
 let mainWindow
 let login
@@ -12,7 +13,7 @@ function createWindow () {
       nodeIntegration: true,
       enableRemoteModule: true
       // "devTools": false,
-    },
+    }
   })
 
   mainWindow.loadFile('index.html')
@@ -39,7 +40,6 @@ function loginWindow () {
 
 app.whenReady().then(() => {
   loginWindow()
-
   // app.on('activate', function () {
   //   if (BrowserWindow.getAllWindows().length === 0) createWindow()
   // })
@@ -59,4 +59,83 @@ ipcMain.on('auth', (event, arg) => {
     mainWindow.hide()
     login.show()
   }
+})
+
+ipcMain.on('fb-authenticate', function (event, arg) {
+  var options = {
+    client_id: '3008138566177268',
+    scopes: 'email',
+    redirect_uri: 'https://www.facebook.com/connect/login_success.html'
+  }
+
+  var authWindow = new BrowserWindow({
+    height: 500,
+    width: 500,
+    show: false,
+    parent: login,
+    modal: true,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  })
+  // authWindow.maximize()
+  var facebookAuthURL = `https://www.facebook.com/v3.2/dialog/oauth?client_id=${options.client_id}&redirect_uri=${options.redirect_uri}&response_type=token,granted_scopes&scope=${options.scopes}&display=popup`
+
+  authWindow.loadURL(facebookAuthURL)
+  authWindow.webContents.on('did-finish-load', function () {
+    authWindow.show()
+  })
+
+  var access_token, error
+  var closedByUser = true
+
+  var handleUrl = function (url) {
+    var raw_code = /access_token=([^&]*)/.exec(url) || null
+    access_token = raw_code && raw_code.length > 1 ? raw_code[1] : null
+    error = /\?error=(.+)$/.exec(url)
+
+    if (access_token || error) {
+      closedByUser = false
+      FB.setAccessToken(access_token)
+      FB.api(
+        '/me',
+        {
+          fields: ['id', 'name', 'picture.width(800).height(800)']
+        },
+        function (res) {
+          mainWindow.webContents.executeJavaScript(
+            'document.getElementById("fb-name").innerHTML = " Name: ' +
+              res.name +
+              '"'
+          )
+          mainWindow.webContents.executeJavaScript(
+            'document.getElementById("fb-id").innerHTML = " ID: ' + res.id + '"'
+          )
+          mainWindow.webContents.executeJavaScript(
+            'document.getElementById("fb-pp").src = "' +
+              res.picture.data.url +
+              '"'
+          )
+        }
+      )
+      authWindow.close()
+    }
+  }
+
+  authWindow.webContents.on('will-navigate', (event, url) => handleUrl(url))
+  var filter = {
+    urls: [options.redirect_uri + '*']
+  }
+  session.defaultSession.webRequest.onCompleted(filter, details => {
+    var url = details.url
+    handleUrl(url)
+  })
+
+  authWindow.on(
+    'close',
+    () =>
+      (event.returnValue = closedByUser
+        ? { error: 'The popup window was closed' }
+        : { access_token, error })
+  )
 })
